7ca60014f7b86d277707d569362114f303bddc59
LiamMcAliffe <lmmcauliffe1223@gmail.com>
2025-01-30 13:57:15 -0500

    Removed login button from script.js

--- public/src/script.js
+++ public/src/script.js
@@ -9,11 +9,7 @@ function practiceMode() {
 document.addEventListener("DOMContentLoaded", () => {
     const playButton = document.getElementById("playButton");
     const practiceButton = document.getElementById("practiceButton");
-    const loginButton = document.getElementById("loginButton");
 
     playButton.addEventListener("click", startGame);
     practiceButton.addEventListener("click", practiceMode);
-    loginButton.addEventListener("click", () => {
-        console.log("Login button clicked");
-    });
 });
